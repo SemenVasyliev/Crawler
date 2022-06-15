d40264f15d472bdb96756c2d19cb8e924c8045f1
User <User@DESKTOP-JQ5MF2V>
2022-06-15 19:28:18 +0200

    Добавьте файлы проекта.

---
+++ CrawlerTest/Program.cs
@@ -0,0 +1,8 @@
+﻿using CrawlerLib;
+
+var crawler = new Crawler("nevelingreply.de");
+var links = await crawler.FindUrls(2);
+
+File.WriteAllLines("C:/Users/User/Desktop/pages.txt", links);
+
+Console.WriteLine("Completed!");
